d6d4f12d548bc4768ad8cb7953f2da2f5b0baaad
‘VeeMose’ <hellovaleriemoseley@gmail.com>
2019-06-27 11:52:01 -0700

    trying out functions

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
-var name = prompt ("Hello, what is your name?");
-alert ("Thanks," + " " + name);
+function name () {
+    var name = prompt ("Hello, what is your name?");
+    alert ("Thanks," + " " + name);
+}
 
-var color = prompt ("What color do you like?");
-document.body.style.background = color;
+function color () {
+    var color = prompt ("What color do you like?");
+    document.body.style.background = color;
+}
+name ();
+color ();
