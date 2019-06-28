24f1fca26a0a5d28d8783f90f7f11c3b7e2ae397
‘VeeMose’ <hellovaleriemoseley@gmail.com>
2019-06-28 09:03:13 -0700

    playing with loops

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-function name () {
-    var name = prompt ("Hello, what is your name?");
-    alert ("Thanks," + " " + name);
+// this is the name section
+function askName() {
+    name = prompt ("Hello, what is your name?");
+}
+askName();
+
+var end = 0;
+
+while (end <= 3) {
+        if (name !== "Jim") {
+        askName();
+        end++;
+    } else {
+        alert ("Thanks," + " " + name);
+    break;
+    }
 }
 
-function color () {
+
+//this is the color section
+function nameColor () {
     var color = prompt ("What color do you like?");
     document.body.style.background = color;
 }
-name ();
-color ();
+
+nameColor ();
+
